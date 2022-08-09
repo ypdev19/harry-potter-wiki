a5e2be3769d13d453d715cf658b9d17816d9e9c8
Yuli Petrilli <ypetrilli19@gmail.com>
2022-08-09 17:44:00 -0400

    Add loading spinner
    Add loading component

--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, {useState, useEffect} from "react";
+
+//Components
 import Card from "../components/Card/Card";
 import Filter from "../components/common/filter/Filter";
 import Pagination from '../components/common/pagination/Pagination';
+import LoadingSpinner from "../components/common/loadingSpinner/LoadingSpinner";
 
 const StudentList = () => {
   let [house, updateHouse] = useState("");
   let [fetchedData, updateFetchedData] = useState([]);
 
   const api = house ? `https://hp-api.herokuapp.com/api/characters/house/${house}` : `https://hp-api.herokuapp.com/api/characters/students`;
+  
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async function () {
-      let data = await fetch(api).then((res) => res.json());
+      setLoading(true);
+
+      const data = await fetch(api).then((respose) => respose.json());
       updateFetchedData(data);
       setLoading(false);
     })();
   }, [api]);
 
-  const [loading, setLoading] = useState(true);
+  
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(10);
 
@@ -36,8 +43,8 @@ const StudentList = () => {
             updateHouse={updateHouse}
           />
           <div className="col-lg-8 col-12">
-            <div className="row">            
-              <Card results={currentRecords} />
+            <div className="row">
+              {loading ? <LoadingSpinner /> : <Card results={currentRecords} />}
             </div>
           </div>
         </div>
